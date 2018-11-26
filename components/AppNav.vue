d7342ef953fe2cfc7e77c1ca608c57e4486c3415
chanu <yuyutyanu@yahoo.co.jp>
2018-11-26 15:19:11 +0900

    update

---
+++ components/AppNav.vue
@@ -0,0 +1,34 @@
+<template>
+  <section>
+    <div class="collapse" id="navbarToggleExternalContent">
+      <div class="bg-light p-4">
+        <a href="#" v-for="list in menu">{{list}}</a>
+      </div>
+    </div>
+    <nav class="navbar navbar-light bg-info">
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent"
+              aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+    </nav>
+  </section>
+</template>
+
+<script>
+  export default {
+    props: {
+      menu: {
+        type: Array,
+        default: () => {
+          return []
+        }
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .logo {
+    text-align: left;
+  }
+</style>
